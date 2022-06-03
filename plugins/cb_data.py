3067a2157989c0a30ce8c0a57f4bd749945aa910
Lastdrogz <105853088+Lastdrogz@users.noreply.github.com>
2022-06-03 12:58:45 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -13,7 +13,7 @@ from Script import script
 async def callback(bot, msg: CallbackQuery):
     if msg.data == "help":
         await msg.message.edit(
-            text=HELP_TXT, 
+            HELP_TXT, 
 	reply_markup=InlineKeyboardMarkup([[
           InlineKeyboardButton("🐼 𝐁𝐀𝐂𝐊 🐼",callback_data = "start"), 
 	  InlineKeyboardButton("↪️ 𝐂𝐥𝐨𝐬𝐞 ↩️",callback_data = "cancel")
