62ac694baccc810dca7fc3ecd215f0c5aaceee3b
Lastdrogz <105853088+Lastdrogz@users.noreply.github.com>
2022-06-03 11:54:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,21 +6,22 @@ from helper.database import  insert
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
 	insert(int(message.chat.id))
-	await message.reply_text(text =f"""
-	Hello {message.from_user.first_name }
-	__I am file renamer bot, Please sent any telegram 
-	**Document Or Video** and enter new filenameto rename it__
+	await message.reply_photo(
+        photo="https://telegra.ph/file/e954574ef60c1790caa79.jpg", 
+        caption =f"""
+	𝐈𝐭'𝐬 𝐏𝐨𝐰𝐞𝐫 𝐅𝐮𝐥𝐥 {message.from_user.mention} 😎 𝐅𝐢𝐥𝐞 𝐑𝐞𝐧𝐚𝐦𝐞𝐫 𝐛𝐨𝐭 + 𝐅𝐢𝐥𝐞 𝟐 𝐕𝐢𝐝𝐞𝐨 𝐂𝐨𝐧𝐞𝐫𝐭𝐞𝐫 𝐁𝐎𝐓 𝐰𝐢𝐭𝐡 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 💖 𝐒𝐡𝐚𝐫𝐞 𝐀𝐧𝐝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐔𝐬.....!!!🦋
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup([[
-          InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Mo_Tech_Group"), 
-	  InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/MoTech_YT")
+          InlineKeyboardButton("𝐒𝐮𝐩𝐩𝐨𝐫𝐭" ,url="https://t.me/+MB8a61q_98A3MThl"), 
+	  InlineKeyboardButton("𝐔𝐩𝐝𝐚𝐭𝐞", url="https://t.me/+hR6DpC_xpPBiM2Zl")
           ],[
-          InlineKeyboardButton("🧩 Deploy 🧩", url="https://youtu.be/7ALYUk-rYMc")
+          InlineKeyboardButton("🧩 𝐂𝐨𝐧𝐭𝐚𝐜𝐓 🥷 𝐀𝐝𝐦𝐢𝐍 🧩", url="https://t.me/kr_admin_bot")
+          ],[
+          InlineKeyboardButton("🧞‍♀️ 𝐇𝐞𝐥𝐩 🧞‍♂️",callback_data = "help"), 
+          InlineKeyboardButton("↪️ <b> Cʟᴏsᴇ </b> ↩️",callback_data = "cancel")
           ]]
           )
         )
-
-
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
@@ -33,4 +34,4 @@ async def send_doc(client,message):
        f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
        ,reply_to_message_id = message.message_id,
        reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
-       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
+       ,InlineKeyboardButton(" <b> Cʟᴏsᴇ </b> ",callback_data = "cancel")  ]]))
